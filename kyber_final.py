11eea68577d428aebac60e132cac1581838cd6f3
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2025-04-10 23:34:48 +0200

    fix: kyber_final

--- kyber_final.py
+++ kyber_final.py
@@ -1,21 +1,22 @@
 # SageMath implementation of polynomial multiplication using CRT in Z_q[X]/(X^d + α)
 import numpy as np
 import random
+import time
 # Parámetros de Kyber-1024
-q = 3329  # Módulo primo
-R = IntegerModRing(q)  # Anillo de enteros módulo q
-n = 256  # Dimensión del polinomio
-PR.<X> = PolynomialRing(R)  # Anillo de polinomios en Z_q
-Rk = PR.quotient(X^n + 1)  # Anillo cociente Z_q[X]/(X^n + 1)
-k = 4  # Parámetro de Kyber
-eta = 2  # Parámetro de ruido binomial
-du, dv = 11, 5  # Parámetros de compresión
+q = 3329  
+R = IntegerModRing(q)  
+n = 256  
+PR.<X> = PolynomialRing(R)  
+Rk = PR.quotient(X^n + 1)  
+k = 4  
+eta1, eta2 = 2, 2
+du, dv = 11, 5  
 
 class ResidueTreeNode:
     def __init__(self, residue):
-        self.residue = residue  # Residuo almacenado en el nodo
-        self.left = None  # Hijo izquierdo
-        self.right = None  # Hijo derecho
+        self.residue = residue  
+        self.left = None  
+        self.right = None  
 
 class ResidueTree:
     def __init__(self, root_residue):
@@ -45,63 +46,71 @@ class ResidueTree:
         if node.right:
             self.display(node.right, level + 1)
 
-
-
-r_list=[]
-def build_residue_tree(tree, node, mod_value, depth):
+def build_residue_tree(tree, node, mod_value, depth, r_list):
     if depth == 2:
         r_list.append(node.residue)
         return
 
     try:
-        r1 = mod(-node.residue, mod_value).sqrt()  # r1 = sqrt(mod(-r, q))
+        r1 = mod(-node.residue, mod_value).sqrt()  
         r_inv = R(r1)^-1  # r1^-1
-        ri = (node.residue * r_inv) % mod_value  # ri = r * r1^-1
-
+        ri = (node.residue * r_inv) % mod_value  
         tree.insert(node.residue, left_residue=ri, right_residue=r1)
-
         left_node = tree.find(tree.root, r1)
         right_node = tree.find(tree.root, ri)
-        build_residue_tree(tree, left_node, mod_value, depth//2)
-        build_residue_tree(tree, right_node, mod_value, depth//2)
+        build_residue_tree(tree, left_node, mod_value, depth//2, r_list)
+        build_residue_tree(tree, right_node, mod_value, depth//2, r_list)
 
     except ValueError:
         return
     
-def NTT_prev(poly1, node, depth, list_):
+def build_tree(alpha, degree):
+    r_list = []
+    tree = ResidueTree(alpha)
+    build_residue_tree(tree, tree.root, q, degree, r_list)
+    return tree, r_list
+
+def NTT_prev(a, node, depth, list_):
     if depth == 2:
-        new_poly = PR(poly1.list()) % ( X^depth + node.residue)
-        list_.append(new_poly)
+        list_.append(a[0]+a[1]*X)
         return None
     else:
-        d=depth//2
-        new_poly1 = PR(poly1.list()) % ( X^depth + node.residue)
-        b = NTT_prev(new_poly1, node.left, d, list_) 
-        c = NTT_prev(new_poly1, node.right, d, list_) 
+        d = depth // 2
+        r=node.right
+        r=R(r.residue)
+        b = [0]*d
+        c = [0]*d
+        poly = a
+        for i in range(d):
+            b[i] = (poly[i] + r *poly[i + d])
+            c[i] = (poly[i] - r *poly[i + d])
+            
+        NTT_prev(b, node.left, d, list_) 
+        NTT_prev(c, node.right, d, list_) 
         return None
 
 def INTT_prev(ntt_list, node, depth):
+    a=[0]*depth
+    r=node.right
+    r=R(r.residue)
+    d = depth//2
+    
     if depth==4:
-        a=[0]*depth
-        r=node.right
-        r=R(r.residue)
         b, c = ntt_list
-        for i in range(0, depth//2):
-            a[i] = (b[i] + c[i]) / 2
-            a[i + depth//2] = (r^(-1) * (b[i] - c[i])) / 2
+        degree_half = n // 2
+        for i in range(0, d):
+            a[i] = (b[i] + c[i]) / degree_half
+            a[i + depth//2] = (r^(-1) * (b[i] - c[i])) / degree_half
         return PR(a)
     else:
-        b = INTT_prev(ntt_list[:depth//4], node.left, depth//2) 
-        c = INTT_prev(ntt_list[depth//4:], node.right, depth//2) 
-        r = node.right
-        r=r.residue
-        a = [0] * depth  
-
-        for i in range(0, depth//2):
-            a[i] = (b[i] + c[i]) / 2
-            a[i + depth//2] = (r^(-1) * (b[i] - c[i])) / 2
+        b = INTT_prev(ntt_list[:d//2], node.left, depth//2) 
+        c = INTT_prev(ntt_list[d//2:], node.right, depth//2) 
+        for i in range(0, d):
+            a[i] = (b[i] + c[i])
+            a[i + d] = (r^(-1) * (b[i] - c[i]))
 
         return PR(a)  
+    
 def NTT(poly):
     list_ = []
     NTT_prev(poly, tree.root, n, list_)
@@ -110,14 +119,22 @@ def NTT(poly):
 def INTT(poly):
     result = INTT_prev(poly, tree.root, n)
     return result
+
+def trivial_multiplication(p, q):
+    resultado = 0
+    for exp_p, coef_p in p.dict().items():
+        for exp_q, coef_q in q.dict().items():
+            coef_p = R(coef_p)
+            coef_q = R(coef_q)
+            resultado += coef_p * coef_q * X**(exp_p + exp_q)
+    return resultado
+    
 def multiply(a, b):
     list_ntt=[]
-    
     listt = r_list[::-1]
     for i in range(0, n//2):
-        list_ntt.append((a[i]*b[i]) % (X^2+listt[i]))
+        list_ntt.append(trivial_multiplication(a[i],b[i]) % (X^2+listt[i]))
     
-    #intt = INTT(list_ntt)
     return list_ntt
 
 
@@ -130,19 +147,15 @@ def mod_switching(x, p, q1):
 def binomial(eta):
     return sum(random.randint(0, 1) for _ in range(eta)) - sum(random.randint(0, 1) for _ in range(eta))
 
-def binomial_sample(eta1):
-    return Rk([binomial(eta1) for _ in range(n)])
-tree = ResidueTree(1)
+def binomial_sample(eta):
+    return Rk([binomial(eta) for _ in range(n)])
 
-build_residue_tree(tree, tree.root, q, depth=n)
-
-# Implementación de multiplicación manual usando bucles for
 def multiply_vector_matrix(v, M):
     resultado = []
     for j in range(k): 
         suma = []
         for i in range(k):  
-            suma.append(multiply(v[i], M[j][i])) #v[i] * M[j, i]
+            suma.append(multiply(v[i], M[j][i])) 
         sum_ = [sum([suma[j][i] for j in range(k)]) for i in range(n//2)]
         resultado.append(sum_)
     return resultado
@@ -154,30 +167,47 @@ def multiply_vector_vector(v1, v2):
 
 def generate_ntt():
     return [R(random.randint(0, q - 1)) + R(random.randint(0, q - 1)) * X for _ in range(n//2)]
-# Key generation (sin cambios en la estructura)
+
+def create_matrix_ntt():
+    return [[generate_ntt() for _ in range(k)] for _ in range(k)]
+
+def create_vector(eta, dimension):
+    return [binomial_sample(eta) for _ in range(dimension)]
+
+def sum_vectors(a, b):
+    return [a[i] + b[i] for i in range(k)] 
+
+def matrix_traspose(matrix):
+    return [[matrix[j][i] for j in range(k)] for i in range(k)]
+
+def NTT_vector(vector):
+    return [NTT(i) for i in vector]
+
+def INTT_vector(vector):
+    return [INTT(i) for i in vector]
+
 def keygen():
-    A_ntt = [[generate_ntt() for _ in range(k)] for _ in range(k)]
-    s = [binomial_sample(eta) for _ in range(k)]
-    e = [binomial_sample(eta) for _ in range(k)]
-    #A_ntt =  [[NTT(A[i][j]) for j in range(k)] for i in range(k)]
+    A_ntt = create_matrix_ntt()
+    s = create_vector(eta1, k)
+    e = create_vector(eta1, k) 
     s_ntt = [NTT(i) for i in s]
     e_ntt = [NTT(i) for i in e]
     s_A_ntt = multiply_vector_matrix(s_ntt, A_ntt)
     
-    t_ntt = [s_A_ntt[i] + e_ntt[i] for i in range(k)] #multiply_vector_matrix(s_ntt, A_ntt) + e_ntt
+    t_ntt = sum_vectors(s_A_ntt, e_ntt) 
     
     return (A_ntt, t_ntt), s_ntt
 
 def encrypt(pk, m):
     A, t = pk
-    r = [binomial_sample(eta) for _ in range(k)]
-    e1 = [binomial_sample(eta) for _ in range(k)]
-    e2 = binomial_sample(eta)
-    r_ntt = [NTT(i) for i in r]
-    A_t = [[A[j][i] for j in range(k)] for i in range(k)]
+    r = create_vector(eta1, k) 
+    e1 = create_vector(eta2, k) 
+    e2 = create_vector(eta2, 1)[0] 
+    r_ntt = NTT_vector(r) 
+    A_t = matrix_traspose(A)
     r_A = multiply_vector_matrix(r_ntt, A_t)
-    r_A_intt = [INTT(i) for i in r_A]
-    u = [r_A_intt[i] + e1[i] for i in range(k)] #multiply_vector_matrix(r, A_t) + e1
+    r_A_intt = INTT_vector(r_A) #[INTT(i) for i in r_A]
+    u = sum_vectors(r_A_intt, e1) 
     r_t_intt = INTT(multiply_vector_vector(r_ntt, t))
     v =  r_t_intt + e2 + (q // 2) * Rk(m)
     
@@ -190,7 +220,7 @@ def decrypt(sk, cipher):
     u, v = cipher  
     u_ = [mod_switching(x, q, 2^du) for x in u]
     v_ = mod_switching(v, q, 2^dv)
-    u_ntt = [NTT(i) for i in u_]
+    u_ntt = NTT_vector(u_) #[NTT(i) for i in u_]
     u_sk = multiply_vector_vector(u_ntt, sk)
     u_sk_intt = INTT(u_sk)
     m_dec = v_ - u_sk_intt
@@ -198,13 +228,16 @@ def decrypt(sk, cipher):
 
     return Rk(m_dec_coeffs)
 
+
+t0 = time.time()
+tree, r_list = build_tree(1, n)
 pk, sk = keygen()
 m = list(np.random.randint(0, 2, n))
-print(f"Mensaje original: {m}")
 
 ciphertext = encrypt(pk, m)
 m_decrypted = decrypt(sk, ciphertext)
-
-print(f"Mensaje desencriptado: {m_decrypted}")
-
-print("¿Coinciden el mensaje original y el desencriptado?:", m == [int(c) for c in m_decrypted.list()])
+tiempo_total = time.time() - t0
+print(f"Original message: {m}")
+print(f"Decrypted message: {m_decrypted}")
+print("Does the original message match the decrypted one?:", m == [int(c) for c in m_decrypted.list()])
+print(f"Elapsed real time: {tiempo_total:.6f} seconds")
